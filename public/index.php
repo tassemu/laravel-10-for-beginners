ec76f639b172514e2dfbecc7533c0a640fa0085c
tassemu <67049566+tassemu@users.noreply.github.com>
2023-11-02 13:05:54 +0100

    ut3

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ define('LARAVEL_START', microtime(true));
 
 /*
 |--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance
+| Checkkkkkkkkk If The Application Is Under Maintenance
 |--------------------------------------------------------------------------
 |
 | If the application is in maintenance / demo mode via the "down" command
